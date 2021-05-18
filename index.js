543d4bbe57585132f0d36dbdfb75ee10d667f144
Varun Srivastava <76582906+varunsr-22737@users.noreply.github.com>
2021-05-18 15:53:09 +0530

    Initialized repository

---
+++ index.js
@@ -0,0 +1,18 @@
+function onHoverMenuItemOne(){
+    document.getElementById('hideMenuItemOne').style.display='block';
+}
+
+function noHoverMenuItemOne() {
+    document.getElementById('hideMenuItemOne').style.display = 'none';
+
+}
+
+function onHoverMenuItemTwo(){
+    document.getElementById('hideMenuItemTwo').style.display='block';
+}
+
+function noHoverMenuItemTwo() {
+    document.getElementById('hideMenuItemTwo').style.display = 'none';
+
+}
+
